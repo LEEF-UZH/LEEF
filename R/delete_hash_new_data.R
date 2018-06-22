61dd694db392cd1cfd9e07baffb6df177fa6852a
Rainer M. Krug <Rainer@krugs.de>
2018-06-22 14:20:22 +0200

    Add function to delete all hash files in the ToBeOImported directory

---
+++ R/delete_hash_new_data.R
@@ -0,0 +1,16 @@
+#' Delete all hash files
+#'
+#' Deleta all \codee{*.sha256} files in the \code{get_option("to_be_imported")} folder recursively.
+#' @return invisibly the result from \code{unlink}
+#' @export
+#'
+#' @examples
+delete_hash_new_data <- function() {
+  fn <- list.files(
+    path = get_option("to_be_imported"),
+    pattern = "*.sha256",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+  return( unlink( fn ) )
+}
