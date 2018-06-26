d4fa4a5d3b7c5f8fc7ac6e583c6dd2916bc308d5
Rainer M. Krug <Rainer@krugs.de>
2018-06-26 12:51:44 +0200

    Safer processing in import_new_data()
    - import_new_data now creates copies of data, processes them and only finally copies the results (archiving) back
    
    - add progress files in to_be_imported directory

--- R/import_new_data.R
+++ R/import_new_data.R
@@ -15,65 +15,92 @@ import_new_data <- function(
   create_new_table = FALSE,
   ...
 ) {
+  # Move data into temporary folder for impoirt and set lock file -----------
+  lockfile <- file.path( get_option( "to_be_imported" ), "LOCKFILE.importing" )
+  set_option( "to_be_imported_org", get_option("to_be_imported") )
+  set_option( "archive_org", get_option("archive") )
+  on.exit(
+    {
+      set_option( "to_be_imported", get_option("to_be_imported_org") )
+      set_option( "to_be_imported_org", NULL )
+      #
+      set_option( "archive", get_option("archive_org") )
+      set_option( "archive_org", NULL )
+      #
+      unlink( lockfile )
+    }
+  )
+  file.create( lockfile )
+  #
+  set_option( "archive", paste0( tempfile( pattern = "Archive.") ) )
+  dir.create( get_option("archive") )
+  #
+  set_option( "to_be_imported", paste0( tempfile( pattern = "ToBeImported.")  ) )
+  dir.create( get_option("to_be_imported") )
+  ## TODO - REPLACE WITH MOVE WHEN FINISHED DEBUGGING!!!!!!!
+  file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.copying_to_be_imported") )
+  file.copy(
+    from = file.path( get_option("to_be_imported_org"), "." ),
+    to = get_option("to_be_imported"),
+    recursive = TRUE
+  )
+  unlink( file.path( get_option( "to_be_imported" ), "IMPORT.copying_to_be_imported") )
+  unlink( file.path( get_option( "to_be_imported" ), "LOCKFILE.importing") )
+  #
+  unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.copying_to_be_imported") )
+
+# Check data, and if TRUE import ------------------------------------------
 
   check <- check_new_data(...)
-  if (check$OK) {
-
+  if (check) {
 
 # Pre-process data --------------------------------------------------------
 
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.pre_process_new_data") )
     pre_process_new_data()
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.pre_process_new_data") )
 
 # Create hashes of new data ---------------------------------------
 
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.hash_new_data") )
     hash_new_data( tts = FALSE, overwrite = FALSE )
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.hash_new_data") )
 
 # Create archive tar ------------------------------------------------------
 
-    tryCatch(
-      archive <- archive_new_data( ),
-      error = function(e) {
-        hf <- list.files(
-          path = get_option("to_be_imported"),
-          recursive = TRUE, pattern = "*.sha256",
-          full.names = TRUE
-        )
-        unlink( hf )
-        print("Error in archive_new_data - hashes deleted!")
-        stop( e )
-      }
-    )
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.archive_new_data") )
+    archive_new_data()
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.archive_new_data") )
 
 # Remove Hashes for further processing ------------------------------------
 
-delete_hash_new_data()
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.delete_hash_new_data") )
+    delete_hash_new_data()
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.delete_hash_new_data") )
 
 # Prepare new data for adding to database ---------------------------------
 
-    if (!extract_new_data()) {
-      hf <- c(
-        list.files(
-          path = get_option("to_be_imported"),
-          recursive = TRUE, pattern = "*.sha256",
-          full.names = TRUE
-        ),
-        archive,
-        paste0(archive, ".sha256")
-      )
-      unlink( hf )
-      stop("There was an error in preparing the data for the import into the database!\n\nImport aborted and created files deleted!")
-    }
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.extract_new_data") )
+    extract_new_data()
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.extract_new_data") )
 
 # import new data ---------------------------------------------------------
 
-    add_new_data( create_new_table = create_new_table )
-
-# Delete new data files ---------------------------------------------------
-
-    delete_new_data()
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.add_new_data") )
+    # add_new_data( create_new_table = create_new_table )
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.add_new_data") )
 
 # Finalize ----------------------------------------------------------------
 
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.copy_archive") )
+    file.copy(
+      from = file.path( get_option("archive"), "."),
+      to = get_option("archive_org"),
+      recursive = TRUE
+    )
+    unlink( file.path( get_option( "to_be_imported_org" ), "IMPORT.copy_archive") )
+
+    file.create( file.path( get_option( "to_be_imported_org" ), "IMPORT.completed") )
   }
   invisible(check)
 }
