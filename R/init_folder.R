29d848aecab0b633e992ad7932239535cc151856
Rainer M. Krug <Rainer@krugs.de>
2018-06-27 10:55:50 +0200

    Add init_folder() function to create needed folder structure

---
+++ R/init_folder.R
@@ -0,0 +1,30 @@
+#' Create folder structure for data import and processing
+#'
+#' @return invisible \code{TRUE}
+#' @export
+#'
+#' @examples
+init_folder <- function(){
+
+# ToBeImported folder structure -------------------------------------------
+  dir.create(            get_option("to_be_imported"),                    showWarnings = FALSE )
+  dir.create( file.path( get_option("to_be_imported"), "bemovi" ),        showWarnings = FALSE )
+  dir.create( file.path( get_option("to_be_imported"), "flowcam" ),       showWarnings = FALSE )
+  dir.create( file.path( get_option("to_be_imported"), "flowcytometer" ), showWarnings = FALSE )
+  dir.create( file.path( get_option("to_be_imported"), "incubatortemp" ), showWarnings = FALSE )
+  dir.create( file.path( get_option("to_be_imported"), "manualcount" ),   showWarnings = FALSE )
+  dir.create( file.path( get_option("to_be_imported"), "respirator" ),    showWarnings = FALSE )
+
+# Archive folder structure ------------------------------------------------
+
+  dir.create( get_option("archive"), showWarnings = FALSE )
+
+# LastAdded folder structure ----------------------------------------------
+
+  dir.create( get_option("last_added"), showWarnings = FALSE )
+
+# Database folder structure -----------------------------------------------
+
+  # dir.create( get_option("archive"), showWarnings = FALSE )
+
+}
