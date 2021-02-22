976219aadefab2b4901e4d8d94d3b124d11e4097
Rainer M. Krug <Rainer@krugs.de>
2021-02-22 15:25:27 +0100

    Add versin to list LEEF packages

--- R/list_LEEF_packages.R
+++ R/list_LEEF_packages.R
@@ -4,6 +4,7 @@
 #' which = "all", recursive = TRUE)} which returns only the packages which
 #' contain \bold{.LEEF} or \bold{LEEF.} and the package \bold{LEEF} itself.
 #' @param recursive logical: should (reverse) dependencies of (reverse) dependencies (and so on) be included? defaults to \code{TRUE}
+#' @param versions logical: should versions be returned as well.
 #'
 #' @return list of all packages which are installed which contain \bold{.LEEF} or \bold{LEEF.} and the package \bold{LEEF} itself
 #' @export
@@ -15,14 +16,26 @@
 #' list_LEEF_packages()
 #' }
 list_LEEF_packages <- function(
-  recursive = TRUE
+  recursive = TRUE,
+  versions = FALSE
 ) {
   drat::addRepo("LEEF-UZH")
   result <- tools::package_dependencies("LEEF", which = "all", recursive = recursive)
-  return(
-    c(
-      "LEEF",
-      grep("LEEF.|.LEEF", result$LEEF, value = TRUE)
+  pkgs <- c(
+    "LEEF",
+    grep("LEEF.|.LEEF", result$LEEF, value = TRUE)
+  )
+
+  if (versions) {
+    pkgs <- as.vector(
+      sapply(
+        pkgs,
+        function(x) {
+          paste0(x, ": ", packageVersion(x))
+        }
+      )
     )
+  }
+  return(
   )
 }
